0e6e3b41e7ba4c426ef152e399cded4b724b405f
Dennisse Andrade <daandradem1@uce.edu.ec>
2024-05-29 19:32:30 -0500

    Taller13 v-on y v-for

--- js/app.js
+++ js/app.js
@@ -1,15 +1,43 @@
 console.log(Vue)
 
+const estudiantes = [{nombre:"Dennisse", apellido:"Andrade"},
+                      {nombre:"Diego", apellido:"Rivas"},
+                      {nombre:"Isabel", apellido:"Marin"},
+                      {nombre:"Guillermo", apellido:"Valencia"},
+                      {nombre:"Taehyung", apellido:"Kim"}
+                    ];
+
+
 const app = Vue.createApp({
-    template: `<h1> Hola Mundo desde Vue.JS </h1>
+    //template: `
+    //<h1> Hola Mundo desde Vue.JS </h1>
+    //<p>{{2+2}}</p>
+    //<p>{{[1,2,3,4]}}</p>
+    //<p>{{false?'True':'False'}}</p>
+    //`
 
-    <p>{{2+2}}</p>
-    <p>{{[1,2,3,4]}}</p>
-    <p>{{false?'True':'False'}}</p>
-    
+    data(){
+        return{
+            mensaje: "Hola mundo propiedad reactiva",
+            valor: 7,
+            lista: estudiantes
+        }
+    },
 
-    `
-    
+    methods:{
+        cambiarTexto(){
+            this.mensaje ="Texto cambiado";
+
+        },
+        sumar(){
+            this.valor = this.valor+5;
+        },
+
+        agregar(){
+            const nuevo = {nombre:"Dennisse", apellido:"Andrade"};
+           this.lista.unshift(nuevo); 
+        }
+    }
 });
 
 app.mount('#myApp');   //puede ser cualquier nombre pero que sea relacionado con el programa
